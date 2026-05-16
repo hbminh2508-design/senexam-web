c73e2c2135fb5e5d643d1389f87ae1f0c822c9eb
uet-bmhoang <hbminh2508@gmail.com>
2026-05-16 07:36:27 +0000

    Cấu hình chuyển hướng tự động sang dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard', // Hoặc đổi thành '/login' nếu bạn muốn bắt đăng nhập trước
+        permanent: true, // Báo cho trình duyệt biết đây là chuyển hướng vĩnh viễn (301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
